6f85a7e2dba0c005bdb91a2765b7804a9761379f
Hanri <henry018@laposte.net>
2022-06-05 15:41:07 +0200

    6. stream controller

--- lib/main.dart
+++ lib/main.dart
@@ -24,68 +24,41 @@ class MyApp extends StatelessWidget {
 }
 
 const String url = "https://bit.ly/3x7J5Qt";
-const double imageHeihgt = 300;
-
-extension Normalize on num {
-  num normalized(num selfRangeMin, num selfRangeMax, [num normalizedRangeMin = 0, num normalizedRangeMax = 1]){
-    return (normalizedRangeMax - normalizedRangeMin) * ((this - selfRangeMin) / (selfRangeMax - selfRangeMin)) + normalizedRangeMin;
-  }
-}
 
 class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final opacity = useAnimationController(
-      duration: const Duration(seconds: 1),
-      initialValue: 1,
-      upperBound: 1,
-      lowerBound: 0
-    );
-    final size = useAnimationController(
-        duration: const Duration(seconds: 1),
-        initialValue: 1,
-        upperBound: 1,
-        lowerBound: 0
-    );
-    final scrollController = useScrollController();
-    useEffect(() {
-     scrollController.addListener(() {
-       final newOpacity = max(imageHeihgt - scrollController.offset, 0.0);
-       final normalized = newOpacity.normalized(0, imageHeihgt).toDouble();
-       opacity.value = normalized;
-       size.value = normalized;
-     });
-    }, [scrollController]);
 
+    late final StreamController<double> controller;
+    controller = useStreamController<double>(
+      onListen: () {
+        controller.sink.add(0.0);
+      }
+    );
     return Scaffold(
-      body: Column(
-        children: [
-          SizeTransition(
-            sizeFactor: size,
-            axis: Axis.vertical,
-            axisAlignment: -1,
-            child: FadeTransition(
-              opacity: opacity,
-              child: Image.network(
-                url,
-                height: imageHeihgt,
-                fit: BoxFit.cover,
-              ),
-            ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              controller: scrollController,
-              itemCount: 100,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(title: Text("Person ${index+1}"));
+      body: StreamBuilder<double>(
+        stream: controller.stream,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          } else {
+            final rotation = snapshot.data ?? 0;
+            return GestureDetector(
+              onTap: () {
+                controller.sink.add(rotation + 10);
               },
+              child: RotationTransition(
+                turns: AlwaysStoppedAnimation(rotation / 360),
+                child: Center(
+                  child: Image.network(url),
+                ),
+              ),
+            );
+          }
 
-            ),
-          )
-        ],
-      ),
+        }
+      )
     );
   }
 }
