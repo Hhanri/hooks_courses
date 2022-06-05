52ed531a0a1c5888c776c76d332e9d69185aa830
Hanri <henry018@laposte.net>
2022-06-05 15:19:10 +0200

    5. scroll, opacity and height animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -22,18 +23,12 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class CountDown extends ValueNotifier<int> {
-  late StreamSubscription sub;
-  CountDown({required int from}) : super(from) {
-    sub = Stream
-        .periodic(const Duration(seconds: 1), (value) => from - value )
-        .takeWhile((value) => value >= 0)
-        .listen((value) {this.value = value;});
-  }
-  @override
-  void dispose() {
-    sub.cancel();
-    super.dispose();
+const String url = "https://bit.ly/3x7J5Qt";
+const double imageHeihgt = 300;
+
+extension Normalize on num {
+  num normalized(num selfRangeMin, num selfRangeMax, [num normalizedRangeMin = 0, num normalizedRangeMax = 1]){
+    return (normalizedRangeMax - normalizedRangeMin) * ((this - selfRangeMin) / (selfRangeMax - selfRangeMin)) + normalizedRangeMin;
   }
 }
 
@@ -41,12 +36,56 @@ class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final countDown = useMemoized(() => CountDown(from: 20));
-    final notifier = useListenable(countDown);
+    final opacity = useAnimationController(
+      duration: const Duration(seconds: 1),
+      initialValue: 1,
+      upperBound: 1,
+      lowerBound: 0
+    );
+    final size = useAnimationController(
+        duration: const Duration(seconds: 1),
+        initialValue: 1,
+        upperBound: 1,
+        lowerBound: 0
+    );
+    final scrollController = useScrollController();
+    useEffect(() {
+     scrollController.addListener(() {
+       final newOpacity = max(imageHeihgt - scrollController.offset, 0.0);
+       final normalized = newOpacity.normalized(0, imageHeihgt).toDouble();
+       opacity.value = normalized;
+       size.value = normalized;
+     });
+    }, [scrollController]);
+
     return Scaffold(
-      body: Center(
-        child: Text(notifier.value.toString())
-      )
+      body: Column(
+        children: [
+          SizeTransition(
+            sizeFactor: size,
+            axis: Axis.vertical,
+            axisAlignment: -1,
+            child: FadeTransition(
+              opacity: opacity,
+              child: Image.network(
+                url,
+                height: imageHeihgt,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              controller: scrollController,
+              itemCount: 100,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(title: Text("Person ${index+1}"));
+              },
+
+            ),
+          )
+        ],
+      ),
     );
   }
 }
