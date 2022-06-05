aba4925a567d28c46cbaae0e9f68c29836401961
Hanri <henry018@laposte.net>
2022-06-05 13:00:21 +0200

    3. future image loader

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
@@ -29,23 +30,33 @@ class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final controller = useTextEditingController();
-    final text = useState('');
-    useEffect(() {
-      controller.addListener(() {
-        text.value = controller.text;
-      });
-    }, [controller]);
+    
+    
+    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
+        .load(url)
+        .then((data) => data.buffer.asUint8List())
+        .then((data) => Image.memory(data)
+    ));
+    final snapshot = useFuture(future);
+
     return Scaffold(
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(text.value),
-          TextField(
-            controller: controller,
-          )
-        ],
+          snapshot.data
+        ].compactMap().toList(),
       ),
     );
   }
 }
+
+
+extension CompactMap<T> on Iterable<T?> {
+  Iterable<T> compactMap<E>([
+    E? Function(T?)? transform
+  ]) => map(
+    transform ?? (e) => e
+  ).where((element) => element != null).cast();
+}
+
+const String url = "https://www.okvoyage.com/wp-content/uploads/2021/02/paysages-norvege-810x538.jpeg";
