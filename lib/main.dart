64e999cf6d01347f96533fd8d005c661b03bc0df
Hanri <henry018@laposte.net>
2022-06-05 14:59:19 +0200

    4. count down

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() {
@@ -21,42 +22,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
-Stream<String> getTime() => Stream.periodic(
-  const Duration(seconds: 1),
-  (_) => DateTime.now().toIso8601String()
-);
+class CountDown extends ValueNotifier<int> {
+  late StreamSubscription sub;
+  CountDown({required int from}) : super(from) {
+    sub = Stream
+        .periodic(const Duration(seconds: 1), (value) => from - value )
+        .takeWhile((value) => value >= 0)
+        .listen((value) {this.value = value;});
+  }
+  @override
+  void dispose() {
+    sub.cancel();
+    super.dispose();
+  }
+}
 
 class MyHomePage extends HookWidget {
   const MyHomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    
-    
-    final future = useMemoized(() => NetworkAssetBundle(Uri.parse(url))
-        .load(url)
-        .then((data) => data.buffer.asUint8List())
-        .then((data) => Image.memory(data)
-    ));
-    final snapshot = useFuture(future);
-
+    final countDown = useMemoized(() => CountDown(from: 20));
+    final notifier = useListenable(countDown);
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          snapshot.data
-        ].compactMap().toList(),
-      ),
+      body: Center(
+        child: Text(notifier.value.toString())
+      )
     );
   }
 }
-
-
-extension CompactMap<T> on Iterable<T?> {
-  Iterable<T> compactMap<E>([
-    E? Function(T?)? transform
-  ]) => map(
-    transform ?? (e) => e
-  ).where((element) => element != null).cast();
-}
-
-const String url = "https://www.okvoyage.com/wp-content/uploads/2021/02/paysages-norvege-810x538.jpeg";
